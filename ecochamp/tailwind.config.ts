c87a3692dcb54fd4b068099f4ba890b82012e347
Adnan Brkic <adohere1@gmail.com>
2025-02-22 14:42:13 +0100

    Initializing default config & Navbar component

--- ecochamp/tailwind.config.ts
+++ ecochamp/tailwind.config.ts
@@ -9,9 +9,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'earthGreen': {
+          100: '#C8E6C9',
+          200: '#ADDAAF',
+          300: '#87C98A',
+          400: '#70BF73',
+          500: '#4CAF50',
+          600: '#459F49',
+          700: '#367C39',
+          800: '#2A602C',
+          900: '#204A22',
+        },
+        'forestGreen': {
+          100: '#BED1C3',
+          200: '#9EBBA7',
+          300: '#729C7E',
+          400: '#568965',
+          500: '#2C6B3F',
+          600: '#286139',
+          700: '#1F4C2D',
+          800: '#183B23',
+          900: '#122D1A',
+        },
       },
+    },
+    fontFamily: {
+      ubuntu: ['Ubuntu', 'serif'],
     },
   },
   plugins: [],
