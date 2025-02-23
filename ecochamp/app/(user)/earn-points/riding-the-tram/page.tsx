8ecf774191ea4359af60678376df5470e4d4d45f
Adnan Brkic <adohere1@gmail.com>
2025-02-23 06:59:19 +0100

    Last commit

--- ecochamp/app/(user)/earn-points/riding-the-tram/page.tsx
+++ ecochamp/app/(user)/earn-points/riding-the-tram/page.tsx
@@ -169,7 +169,7 @@ export default function Page() {
                         </div>
                     )}
                         
-                </Modal>
+            </Modal>
         </div>
     )
 }
